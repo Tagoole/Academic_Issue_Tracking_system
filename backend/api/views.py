128d47256ab0279d0b7b020a95c224b85831e357
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-04-02 11:21:32 +0300

    Registrar can now also filter the issues

--- backend/api/views.py
+++ backend/api/views.py
@@ -144,6 +144,20 @@ class Registrar_Issue_ManagementViewSet(ModelViewSet):
     def perform_destroy(self, instance):
         self.send_email_on_update(instance,"deleted")
         instance.delete()
+        
+    @action(detail = False, methods= ['get'])
+    def filter_results(self,request):
+        search_query = request.query_params.get('status','').strip()
+        if search_query:
+            issues = self.get_queryset().filter(
+                Q(status__icontains = search_query) | 
+                Q(student__username__icontains = search_query) |
+                Q(issue_type__icontains = search_query)
+                )
+            serializer = self.get_serializer(issues, many = True)
+            return Response(serializer.data)
+        return Response({'error':'Status parameter required'})
+    
             
     
     
