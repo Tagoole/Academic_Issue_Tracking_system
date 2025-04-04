cef920ff95cd6393056705db4d990b78fbd65d1a
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-04-04 17:45:13 +0300

    Modified the landing page to be the default but has no profile picture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import ProfileSetup from "./pages/ProfileSetup";
 import ProfilePictureSetup from "./pages/profilepicture";
 import Congragulation from "./pages/congragulation";
 import RegistraDashboard from "./pages/RegistraDashboard";
-import IssueManagement  from "./pages/IssueManagement";
+import IssueManagement from "./pages/IssueManagement";
 import Viewdetails from "./pages/View-details";
 import Changepassword from "./pages/Changepassword";
 import Preferences from "./pages/Preferences";
@@ -22,44 +22,47 @@ import StudentDashboard from "./pages/StudentDashboard";
 import SignUp from "./pages/signup";
 import ProfileScreen from "./pages/StudentsProfile";
 import Notifications from "./pages/Notifications";
-
+import LandingPage from "./pages/landingpage";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          
-          
-          
-          <Route path="/notificationsuccess" element={<NotificationSuccess />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/verification" element={<EmailVerification />} />
-          <Route path="/reset" element={<ResetPassword />} />
-          <Route path="/issues" element={<Issues />} />
-          <Route path="/profilesetup" element={<ProfileSetup />} />
-          <Route path="/profilepicture" element={<ProfilePictureSetup />} />
-          <Route path="/congragulation" element={<Congragulation />} />
-          {<Route path="/new-issue" element={<NewIssue />} />}
-          <Route path="/RegistraDashboard"element={<RegistraDashboard/>}/>
-          {<Route path ="/issuemanagement"element={<IssueManagement/>}/>}
-          <Route path ="/view-details"element={<Viewdetails/>}/>
-          <Route path="/changepassword"element={<Changepassword/>}/>
-          <Route path="/Preferences"element={<Preferences/>}/>
-          <Route path="/Help"element={<Help/>}/>
-          <Route path="/Deleteaccount"element={<Deleteaccount/>}/>
-          <Route path="/studentdashboard"element={<StudentDashboard/>}/>
-          <Route path="signup" element={<SignUp/>}/>
-          <Route path="/Studentsprofile"element={<ProfileScreen/>}/>
-          <Route path="/Notifications" element={<Notifications />} />
-       
-          {/* Add more routes as needed */}
-          
-
-          
-        </Routes>
-      </div>
+      <Routes>
+        {/* Set LandingPage as the default route */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/landingpage" element={<LandingPage />} />
+        
+        {/* Authentication routes */}
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/verification" element={<EmailVerification />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/changepassword" element={<Changepassword />} />
+        
+        {/* Profile setup routes */}
+        <Route path="/profilesetup" element={<ProfileSetup />} />
+        <Route path="/profilepicture" element={<ProfilePictureSetup />} />
+        <Route path="/congratulation" element={<Congragulation />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        
+        {/* Dashboard routes */}
+        <Route path="/studentdashboard" element={<StudentDashboard />} />
+        <Route path="/registrardashboard" element={<RegistraDashboard />} />
+        
+        {/* Issue management routes */}
+        <Route path="/new-issue" element={<NewIssue />} />
+        <Route path="/issues" element={<Issues />} />
+        <Route path="/issuemanagement" element={<IssueManagement />} />
+        <Route path="/viewdetails/:id" element={<Viewdetails />} />
+        
+        {/* Other routes */}
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/notification-success" element={<NotificationSuccess />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/preferences" element={<Preferences />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/deleteaccount" element={<Deleteaccount />} />
+      </Routes>
     </Router>
   );
 }
--- frontend/src/pages/landingpage.jsx
+++ frontend/src/pages/landingpage.jsx
@@ -1,42 +1,56 @@
 import React from 'react';
 import './landingpage.css';
 import { useNavigate } from 'react-router-dom';
-import makererelogo from '../assets/makerere.logo.png';
+import makererelogo from '../assets/makererelogo.png';
 import landingimage from '../assets/landingimage.png';
 
 const LandingPage = () => {
-    const navigate = useNavigate();
-
-    const handlesignup = useNavigate();
-        function navigateToSignup() {
-            navigate('/signup');
-    };
-
-        function navigateToSignnin() {
-            navigate('/signin');
-        };
-    return(
-        <div className="landingpage">
-            <img src={landingimage} alt="Landing Image" className="landing-image" />  
-            <div clasName="logo">
-                <h1 className='logo-name'>AITS</h1>
-            </div>  
-            <div className="overlay">
-                <img src={makererelogo} alt="Makerere University Logo" className="makerere-logo" />
-                <div className="welcome-text">WELCOME TO THE ISSUE ACADEMIC TRACKING SYSTEM</div>   
-            </div>   
-             
-            <div className="header">
-                <div className="buttons">
-                <button className='signup-button' onClick={navigateToSignup}>Sign Up</button>
-                <div className='separator'></div>
-                <button className='signin-button' onClick={navigateToSignnin}>Sign In</button>
-                </div>
-            </div>
-           
+  const navigate = useNavigate();
+  
+  const navigateToSignup = () => {
+    navigate('/signup');
+  };
+  
+  const navigateToSignin = () => {
+    navigate('/signin');
+  };
+  
+  return (
+    <div className="landingpage">
+      {/* Using the landingimage as a background directly in JSX */}
+      <div className="bg-image" style={{ backgroundImage: `url(${landingimage})` }}></div>
+      
+      {/* Logo at the top */}
+      <div className="logo">
+        <h1 className="logo-name">AITS</h1>
+      </div>
+      
+      {/* Main content section */}
+      <div className="content-container">
+        <div className="overlay">
+          <img 
+            src={makererelogo} 
+            alt="Makerere University Logo" 
+            className="makerere-logo" 
+          />
+          <div className="welcome-text">
+            WELCOME TO THE ACADEMIC ISSUE TRACKING SYSTEM
+          </div>
+          
+          {/* Buttons now below the welcome text */}
+          <div className="buttons">
+            <button className="signup-button" onClick={navigateToSignup}>
+              Sign Up
+            </button>
+            <div className="separator"></div>
+            <button className="signin-button" onClick={navigateToSignin}>
+              Sign In
+            </button>
+          </div>
         </div>
-    );
-
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
