78ab2120b4ce1a75ee3a2878c2245bc65a9936cf
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-04-04 17:52:56 +0300

    Modified the Newissue.jsx to push and then deployed app works...

--- frontend/src/pages/New-issue.jsx
+++ frontend/src/pages/New-issue.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
 import Sidebar from './Sidebar1';
-import './New-Issue.css';
+import './New-issue.css';
 import backgroundimage from "../assets/backgroundimage.jpg";
 import axios from 'axios'; // Import axios for API calls
 
