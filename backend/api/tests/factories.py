2fa64280db8113360044c75fd70f002eb14eb17d
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2025-04-11 17:08:49 +0300

    Created a base factory for Custom user

--- backend/api/tests/factories.py
+++ backend/api/tests/factories.py
@@ -2,3 +2,6 @@ import factory
 from factory.django import DjangoModelFactory
 from django.contrib.auth.hashers import make_password
 from api.models import *
+
+class CustomUserFactory(DjangoModelFactory):
+    pass
