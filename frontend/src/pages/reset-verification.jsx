4d5451a5bada418811a53d6d4e10d5e8928945e3
Namujuzi Ritah <nritah2024@gmail.com>
2025-04-23 23:32:32 +0300

    enhanced code clarity

--- frontend/src/pages/reset-verification.jsx
+++ frontend/src/pages/reset-verification.jsx
@@ -8,6 +8,7 @@ import './reset-verification.css';
 
 
 const ResetVerification = () => {
+  // State variables and hooks
   const [verificationCode, setVerificationCode] = useState(['', '', '', '', '']);
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
