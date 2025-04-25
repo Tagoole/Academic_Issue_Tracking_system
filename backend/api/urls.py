394fe806100df83607744b564d1460b5a375e25c
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2025-04-25 13:01:11 +0300

    Program view set adusted

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,7 +12,7 @@ router.register(r'issues', IssueViewSet, 'issues')  # Registers IssueViewSet to 
 router.register(r'department', DepartmentViewSet, 'department')  # Registers DepartmentViewSet to handle CRUD operations for departments
 router.register(r'course_unit', Course_unitViewSet, 'course_unit')  # Registers Course_unitViewSet to handle CRUD operations for course units
 router.register(r'program', ProgramViewSet, 'program')  # Registers ProgramViewSet to handle CRUD operations for programs
-router.register(r'registration_token',Registration_Token_viewset,'registration_token')
+router.register(r'registration_token', Registration_Token_viewset, 'registration_token')  # Registers Registration_Token_viewset to handle CRUD operations for registration tokens
 router.register(r'registrar_issue_management',Registrar_Issue_ManagementViewSet,'registrar_issue_management')
 router.register(r'lecturer_issue_management', Lecturer_Issue_Manangement, 'lecturer_issue_management')
 router.register(r'student_issues', Student_Issue_ReadOnlyViewset, 'student_issues')
