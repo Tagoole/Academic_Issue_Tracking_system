576d298d0d271c7220f7ae6ecf7f09582d9a4f3b
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2025-04-25 13:28:28 +0300

    get_registrar path added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('final_password_reset/',final_password_reset,name='final_password_reset'),
     path('user_email_notifications/',get_user_email_notifications,name = 'user_email_notifications'),
     path('resend_password_reset_code/',resend_password_reset_code,name = 'resend_password_reset_code'),
-    path('get_registrars/',get_registrars,name = 'get_registrars'),
+    path('get_registrars/', get_registrars, name='get_registrars'),  # Endpoint to retrieve a list of registrars
     path('get_lecturers/',get_lecturers,name = 'get_lecturers'),
     path('delete_account/',delete_account,name='delete_account'),
 ]
