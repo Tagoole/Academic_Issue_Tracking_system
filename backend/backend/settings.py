640063d508c115c5e2779f53ed9f9b25c438d0d5
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-04-27 19:39:59 +0300

    made modifications to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Adjust to match your React dev server
+    "http://localhost:5173",
+    "https://fluffy-biscuit-9cbf4f.netlify.app"
 ]
 CORS_ALLOWS_CREDENTIALS = True
 
