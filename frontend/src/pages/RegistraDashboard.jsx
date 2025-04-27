d72d6c380526fc7a9ca6b7db1a4305c88806ef40
Ivy256-code <ivynassuuna@gmail.com>
2025-04-27 16:43:13 +0300

    new css is not functioning well

--- frontend/src/pages/RegistraDashboard.jsx
+++ frontend/src/pages/RegistraDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+import Navbar from './NavBar';
+import Sidebar from './Sidebar';
 import './registradashboard.css';
 
 const RegistraDashboard = () => {
