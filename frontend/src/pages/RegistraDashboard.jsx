c74fe89464405dc2fc6bd1a47013bbfb8b1d78be
Ivy256-code <ivynassuuna@gmail.com>
2025-04-27 16:37:53 +0300

    updated the css path of the dashboard

--- frontend/src/pages/RegistraDashboard.jsx
+++ frontend/src/pages/RegistraDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-import './registration-dashboard-styles.css';
+import './registradashboard-styles.css';
 
 const RegistraDashboard = () => {
   return (
