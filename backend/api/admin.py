1684020d75611f3558651d40d7f0a65f9b50eb26
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2025-05-02 22:50:24 +0300

    lecturere field

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -8,7 +8,7 @@ class IssueAdmin(admin.ModelAdmin):  # Custom admin configuration for the Issue 
     def get_fields(self, request, obj=None):  # Customizes the fields displayed in the admin interface based on the user's role
         fields = super().get_fields(request, obj)  # Retrieves the default fields for the admin interface
         if request.user.role == 'student':  # Checks if the logged-in user has the role of 'student'
-            fields = [field for field in fields if field != 'lecturer']
+            fields = [field for field in fields if field != 'lecturer']  # Removes the 'lecturer' field if the user is a student
         return fields
     
     
