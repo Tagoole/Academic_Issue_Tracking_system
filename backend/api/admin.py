1f6ae267e2633ff93cf2bc3f76c9809f9a2ade95
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2025-05-02 22:33:32 +0300

    register verification code

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -36,7 +36,7 @@ admin.site.register(Issue)
 admin.site.register(Department)
 admin.site.register(Course_unit)
 admin.site.register(Registration_Token)
-admin.site.register(Verification_code)
+admin.site.register(Verification_code)  # Registers the Verification_code model with the admin site
 admin.site.register(Email_Notification)  # Registers the Email_Notification model with the admin site
 admin.site.register(Message)  # Registers the Message model with the admin site
 admin.site.register(Conversation)  # Registers the Conversation model with the admin site
