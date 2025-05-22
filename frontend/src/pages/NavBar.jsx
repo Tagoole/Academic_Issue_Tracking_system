a7a26493e7949fc9c04174e5408ca42ab6ac719a
Ivy256-code <ivynassuuna@gmail.com>
2025-05-22 16:02:00 +0300

    added new navbar css

---
+++ frontend/src/pages/NavBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './NavBar.css';
+import logo from '../assets/makererelogo.png'
+import { useNavigate } from 'react-router-dom';
+
+const NavBar = () => {
+  const [userName, setUserName] = useState('User'); // Initialize with a default value
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    // Check for username in localStorage
+    const storedUserName = localStorage.getItem('userName');
+    if (storedUserName) {
+      const capitalizedName = storedUserName.charAt(0).toUpperCase() + storedUserName.slice(1);
+      setUserName(capitalizedName);
+    }
+  }, []);
+
+  return (
+    <nav className="navbar">
+      {/* Left Section: Username greeting */}
+      <div className="username-greeting">
+        <span>Hello, {userName}</span>
+      </div>
+      
+      {/* Right Section: Title and Logo */}
+      <div className="navbar-right">
+        <div className="app-title">Academic Issue Tracking System</div>
+        <img src={logo} alt="Logo" className="logo-image" />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
