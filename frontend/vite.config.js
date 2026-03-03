668b0f8b634f693b9a6d0e4f8967aebde75292fd
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-03-03 12:50:54 +0300

    ......o

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,        // 0.0.0.0 → accept external connections
+    port: 5173,
+    strictPort: true,
+    cors: true,
+    // explicitly allow HTTP
+    https: false,
+    allowedHosts: "all"
+  }
 })
